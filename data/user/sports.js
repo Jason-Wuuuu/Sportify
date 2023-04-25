8c2c83d0635a38398624e8aec781a2dc64759dec
Dhruv267 <47938512+Dhruv267@users.noreply.github.com>
2023-04-25 15:39:47 -0400

    Added get(), getAll() data functions for sports table
    Added validation to create() and added two new data functions which are get(ID) and getAll() in user/sports.js

--- data/user/sports.js
+++ data/user/sports.js
@@ -3,14 +3,33 @@ import { ObjectId } from "mongodb";
 import validation from "./helpers.js";
 
 const create = async (name) => {
+  name = validation.checkString(name, "name");
+
   let newSport = {
     name: name,
   };
 
   const sportCollection = await sports();
   const newInsertInformation = await sportCollection.insertOne(newSport);
+  if (!newInsertInformation.acknowledged || !newInsertInformation.insertedId)
+    throw "Could not add sport!";
   const newId = newInsertInformation.insertedId;
-  await get(newId.toString());
 
-  return { insertedSport: true };
+  let addedSport = await get(newId.toString());
+
+  return { insertedSport: addedSport };
+};
+
+const getAll = async () => {
+  const sportCollection = await sports();
+  const sportList = await sportCollection.find({}).toArray();
+  return sportList;
+};
+
+const get = async (sportID) => {
+  sportID = validation.checkId(sportID);
+  const sportCollection = await sports();
+  const sport = await sportCollection.findOne({ _id: new ObjectId(sportID) });
+  if (!sport) throw "Error: Sport not found";
+  return sport;
 };
