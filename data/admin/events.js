e71aac02ac95701b887309295a32a9cb032896b7
Chia-Hsiang Wu <cwu34@stevens.edu>
2023-04-27 15:39:40 -0400

    bug fixed & completed validations for admin

--- data/admin/events.js
+++ data/admin/events.js
@@ -14,35 +14,4 @@ const remove = async (eventID) => {
   return { deleted: true };
 };
 
-//
-const getAllUsers = async () => {};
-
-const getUnapprovedEvents = async () => {
-  const eventCollection = await events();
-  const unapprovedEventList = await eventCollection
-    .find({
-      approved: false,
-    })
-    .toArray();
-  return unapprovedEventList;
-};
-
-const approve = async (eventID) => {
-  eventID = validation.checkId(eventID);
-
-  const eventUpdateInfo = {
-    approved: true,
-  };
-  const eventCollection = await events();
-  const updateInfo = await eventCollection.findOneAndUpdate(
-    { _id: new ObjectId(eventID) },
-    { $set: eventUpdateInfo },
-    { returnDocument: "after" }
-  );
-  if (updateInfo.lastErrorObject.n === 0)
-    throw `Error: Update failed, could not find a event with id of ${eventID}`;
-
-  return eventUpdateInfo;
-};
-
-export { remove, approve, getUnapprovedEvents };
+export { remove };
