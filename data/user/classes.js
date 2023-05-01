3119587b2684f5d2249943892a7f89e82e213b13
devgupta2399 <devrgupta2399@gmail.com>
2023-05-01 14:49:53 +0530

    Add class functionality is added

--- data/user/classes.js
+++ data/user/classes.js
@@ -1,10 +1,23 @@
 import { classes } from "../../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
+import {helperMethodsForClasses,helperMethodsForUsers} from "./helpers.js";
 
 const reserve = async (classID, userID) => {
   // add user to the student field (an array) in class collection
+  classID = helperMethodsForClasses.checkId(classID, "classID");
+  userID = helperMethodsForUsers.checkId(userID, "userID");
   const classCollection = await classes();
-
+  const classObj = await classCollection.findOne({_id: new ObjectId(classID)});
+  if(classObj.students.includes(userID)) 
+    return { reserved: true, msg: "Already reserved" };
+  const updateInfo = await classCollection.findOneAndUpdate(
+    { _id: new ObjectId(classID) },
+    { $push: { students: userID } },
+    { returnDocument: "after" }
+  );
+  if (updateInfo.lastErrorObject.n === 0)  
+    throw `Error: Update failed, could not find a class with id of ${classID}`;
+  return { reserved: true, msg: "Sucessfully reserved" };
 };
 
 const quit = async (classID, userID) => {};
@@ -18,10 +31,11 @@ const getAllClasses = async () => {};
 const getAllStudents = async (classID) => {};
 
 const getClassesBySport = async (sportID) => {
+  sportID = helperMethodsForClasses.checkId(sportID, "sportID");
   const classCollection = await classes();
-  const classList = await classCollection.find({sportID: sportID}).toArray();
-  return classList;
-};
+  const classesBySport = await classCollection.find({ sportID: sportID }).toArray();
+  return classesBySport;
+};    
 
 const getClassesBySportPlace = async (sportPlaceID) => {};
 
@@ -31,6 +45,4 @@ const getClassesByTime = async (time) => {};
 
 const getAvailableClasses = async () => {}; // classes that the capacity is not full
 
-export {getClassesBySport};
-
-// sorting ?
+export {reserve, getClassesBySport};
